e675424bed854e3950ba5566592ddae7c3bb7cad
Kurt Lekanger <kurt@lekanger.no>
2025-06-01 13:56:31 +0200

    chore: add contact info below accordion

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,10 @@ export default async function Faq() {
   return (
     <article>
       <Accordion items={accordion.items} title={title} />
+      <p className="text-gray-400 font-light text-sm md:text-base">
+        Ble du ikke noe klokere av dette? Ta kontakt på{" "}
+        <a href="mailto:kurt@lekanger.no">kurt@lekanger.no</a>
+      </p>
     </article>
   );
 }
